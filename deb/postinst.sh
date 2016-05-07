10bec10eb19af8e5fe8608b105fce7abced1cd37
Evin Callahan <gaieges@gmail.com>
2016-05-07 13:11:34 -0500

    reorder steps

--- deb/postinst.sh
+++ deb/postinst.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-/usr/sbin/service kapture start
 /bin/chown -R kapture.kapture /var/kapture /etc/kapture
 
 /sbin/iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080
@@ -12,3 +11,5 @@ fi
 if [ -x /etc/init.d/iptables-persistent ]; then
   /usr/sbin/invoke-rc.d iptables-persistent save  > /dev/null 2>&1
 fi
+
+/usr/sbin/service kapture start
