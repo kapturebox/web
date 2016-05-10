016255eaebe4700383e80d1321591c0267e3c275
Evin Callahan <gaieges@gmail.com>
2016-05-10 11:28:36 -0500

    re-install fix

--- deb/prerm.sh
+++ deb/prerm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if [ -f /etc/init/kapture.conf ] && /usr/sbin/service kapture status >/dev/null ; then
+if [ -f /etc/init/kapture.conf ] && /usr/sbin/service kapture status | grep -q running; then
   /usr/sbin/service kapture stop
 fi
