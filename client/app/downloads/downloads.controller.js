7c49281343709499e3e4d99e6a2c05ef41aabb4e
xcezzz <xxcezz@gmail.com>
2016-05-10 15:57:00 -0400

    other crap

--- client/app/downloads/downloads.controller.js
+++ client/app/downloads/downloads.controller.js
@@ -1,20 +1,21 @@
 'use strict';
 
 angular.module('kaptureApp')
-  .controller('DownloadsCtrl', function ($scope, $http, $timeout, $location) {
-
-    (function getDownloads() {
-      $http({
-        method: 'GET',
-        url: '/api/download',
-        timeout: 30000  // in ms
-      }).then( function( resp ) {
-        $scope.downloads = resp.data;
-      }).finally( function() {
-        if( $location.path() === '/downloads' ) {          
-          $timeout( getDownloads, 3000 );
-        }
-      })
-    })();
+  .controller('DownloadsCtrl', function ($scope, $http, $interval, $location, downloadService) {
+    $scope.getDownloads = downloadService.getCurrentDownloads;
+    
+    // (function getDownloads() {
+    //   $http({
+    //     method: 'GET',
+    //     url: '/api/download',
+    //     timeout: 30000  // in ms
+    //   }).then( function( resp ) {
+    //     $scope.downloads = resp.data;
+    //   }).finally( function() {
+    //     if( $location.path() === '/downloads' ) {          
+    //       $timeout( getDownloads, 3000 );
+    //     }
+    //   })
+    // })();
 
   });
