f5e974c077db75879d478ea5e8643db29da71319
Evin Callahan <gaieges@gmail.com>
2017-02-26 17:07:15 -0600

    container prep

--- deb/postinst.sh
+++ deb/postinst.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-/bin/chown -R kapture.kapture /var/kapture /etc/kapture /var/lib/kapture /var/lib/kapture/downloads
-
-/sbin/iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080
-
-if [ -x /etc/init.d/netfilter-persistent ]; then
-  /usr/sbin/invoke-rc.d netfilter-persistent save > /dev/null 2>&1
+if /usr/bin/id kapture; do
+  /bin/chown -R kapture.kapture /var/kapture /etc/kapture /var/lib/kapture /var/lib/kapture/downloads
 fi
 
-if [ -x /etc/init.d/iptables-persistent ]; then
-  /usr/sbin/invoke-rc.d iptables-persistent save  > /dev/null 2>&1
+if /sbin/iptables ; do
+  /sbin/iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080
+
+  if [ -x /etc/init.d/netfilter-persistent ]; then
+    /usr/sbin/invoke-rc.d netfilter-persistent save > /dev/null 2>&1
+  fi
+
+  if [ -x /etc/init.d/iptables-persistent ]; then
+    /usr/sbin/invoke-rc.d iptables-persistent save  > /dev/null 2>&1
+  fi
 fi
+
+
 
 # WOULD BE BETTER TO DO THESE POSTINST SCRIPTS VIA NPM ....
 # since this dep contains a binary that needs to be arch dependent, install it in the dir
@@ -20,6 +26,8 @@ fi
 
 /usr/bin/nodejs /var/kapture/server/node_modules/youtube-dl/scripts/download.js
 
+if systemctl; do
+  /bin/systemctl enable kapture > /dev/null 2>&1
+  /usr/sbin/service kapture start
+fi
 
-/bin/systemctl enable kapture > /dev/null 2>&1
-/usr/sbin/service kapture start
--- deb/postrm.sh
+++ deb/postrm.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # delete rule if exists
-if /sbin/iptables -S PREROUTING -t nat | grep -q -- '-A PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 8080'; then
-  /sbin/iptables -D PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-ports 8080
+if /sbin/iptables; do 
+  if /sbin/iptables -S PREROUTING -t nat | grep -q -- '-A PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 8080'; then
+    /sbin/iptables -D PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-ports 8080
+  fi
 fi
+
